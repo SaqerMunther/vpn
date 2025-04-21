61401bb3de1d48ba43031c2285c9bf83cadb2b06
SaqerMunther <saqermunther321@gmail.com>
2025-04-21 12:12:13 +0300

    Update CacheListener.java

--- com/app/dev/cmon/cache/CacheListener.java
+++ com/app/dev/cmon/cache/CacheListener.java
@@ -25,3 +25,40 @@ public class CacheListener implements ServletContextListener {
         }
     }
 }
+
+
+
+
+
+package com.app.dev.cmon.cache;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@WebListener
+public class CacheContextListener implements ServletContextListener {
+    private static final Logger logger = LoggerFactory.getLogger(CacheContextListener.class);
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        // Force CacheService to initialize on startup
+        CacheService cacheService = CacheService.getInstance();
+        logger.info("CacheService initialized on deploy: cache size = {}", cacheService.size());
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        // Cleanly shut down Ehcache when the webapp is undeployed or server stops
+        CacheService.getInstance().shutdown();
+        logger.info("CacheService shutdown on undeploy");
+    }
+}
+
+
+
+<listener>
+  <listener-class>com.app.dev.cmon.cache.CacheContextListener</listener-class>
+</listener>
