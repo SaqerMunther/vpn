596a5b60066e6e4c5737d538448866f210974f6c
SaqerMunther <saqermunther321@gmail.com>
2025-04-22 12:55:16 +0300

    Update CacheListener.java

--- com/app/dev/cmon/cache/CacheListener.java
+++ com/app/dev/cmon/cache/CacheListener.java
@@ -3,38 +3,6 @@ package com.app.dev.cmon.cache;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
-
-@WebListener
-public class CacheListener implements ServletContextListener {
-    private CacheService cacheService;
-
-    @Override
-    public void contextInitialized(ServletContextEvent sce) {
-        // Initialize the cache service when the context is initialized
-        cacheService = CacheService.getInstance();
-        System.out.println("CacheService initialized.");
-    }
-
-    @Override
-    public void contextDestroyed(ServletContextEvent sce) {
-        // Clear the cache and shutdown the cache manager when the context is destroyed
-        if (cacheService != null) {
-            cacheService.clearCache();
-            cacheService.shutdown(); // Ensure to shutdown the CacheManager
-            System.out.println("CacheService cleared and shutdown.");
-        }
-    }
-}
-
-
-
-
-
-package com.app.dev.cmon.cache;
-
-import javax.servlet.ServletContextEvent;
-import javax.servlet.ServletContextListener;
-import javax.servlet.annotation.WebListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -44,21 +12,13 @@ public class CacheContextListener implements ServletContextListener {
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-        // Force CacheService to initialize on startup
         CacheService cacheService = CacheService.getInstance();
-        logger.info("CacheService initialized on deploy: cache size = {}", cacheService.size());
+        logger.info("CacheService initialized");
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
-        // Cleanly shut down Ehcache when the webapp is undeployed or server stops
         CacheService.getInstance().shutdown();
-        logger.info("CacheService shutdown on undeploy");
+        logger.info("CacheService shutdown");
     }
 }
-
-
-
-<listener>
-  <listener-class>com.app.dev.cmon.cache.CacheContextListener</listener-class>
-</listener>
